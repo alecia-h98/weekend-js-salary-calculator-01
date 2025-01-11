1bc4fb6d4288ef0ffbc56296909c5e60db3c5b83
Alecia Hagen <alecia.h98@gmail.com>
2025-01-11 17:00:43 -0600

    Added an onSubmit button, grabbed user's inputs, set up object and attached my object to my global array.

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 console.log('Hello World');
 
 //employee data array
-const employeeData = [];
+let employeeData = [];
+
+function readyNow(){
+    console.log('DOM IS READY!!');
+}
+
+function onSubmit(event){
+//preventing the webpages default behavior
+    event.preventDefault();
+
+ //grabbing the user's input elements by id(or value attribute. -found in the html form-)
+    const firNameElement = document.getElementById("first-name-input");
+    const lastNameElement = document.getElementById("last-name-input");
+    const idElement = document.getElementById("id-input");
+    const titleElement = document.getElementById("title-input");
+    const salaryElement = document.getElementById("salary-input");
+
+//array/object setup for user's inputs
+ let userInput = {
+        fName: firNameElement.value,
+        lName: lastNameElement.value,
+        id: idElement.value,
+        title: titleElement.value,
+        salary: Number(salaryElement.value),
+    };
+
+ employeeData.push(userInput);
+
+ console.log(employeeData);
+}
